ee1e3a1eccbceb635fa48cd7ee62f12884a0ce69
jadbathore <131593711+jadbathore@users.noreply.github.com>
2024-04-11 17:46:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <!-- 
 divise en model view controller caller.php fais l'appel de toute les breve et les test grace à un try-catch le model Acceuil s'affiche uniquement apres une derniere verification de la table eleveManager 
-la class Bb est a la methode settingdb qui permet la connection par defaut à la base de donnée mais on peux egalement passer des paramettre et changer la base de donner si on le souhaite .
+la class Bb est a la methode settingdb qui permet la connection par defaut à la base de donnée mais on peux egalement passer des paramètres et changer la base de donner si on le souhaite .
 pour la question 4 complementaire je dirait qu'une evolution fonctionnel serait de faire des stat pour les ecole comme ca on pourait les comparé.
 -->
 <html lang="fr">
