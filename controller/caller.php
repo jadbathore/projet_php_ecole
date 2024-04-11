d0193e5226ca63aae73c6a890edde7d0a23045f7
jadbathore <131593711+jadbathore@users.noreply.github.com>
2024-04-11 11:24:15 +0200

    Update caller.php

--- controller/caller.php
+++ controller/caller.php
@@ -20,7 +20,7 @@ $listeStat = array(
     'performanceN_3' => ['stat_id' => 3, 'stat_perf' => 18, 'sport_id' => 2],
     'performanceN_4' => ['stat_id' => 4, 'stat_perf' => 40, 'sport_id' => 1],
 );
-//vous remarquera que certaine sont 0 et d'autre son son des arrays cela et suportter grace a une methode qui identifie chaque cas de figure et procedes en fonction.
+//vous remarquera que certaine sont 0 et d'autre son des arrays cela et suporter grace a une methode qui identifie chaque cas de figure et procedes en fonction.
 
 $listedescr = array(
     'description_paul' => ['id' => 1, 'eleve_id' => 1, 'ecole_id' => 1, 'sport_id' => [1, 2, 4]],
@@ -31,8 +31,13 @@ $listedescr = array(
 );
 
 
-//toute les breve son tester les une apres les autre (foreach array je fait une breve et en suite j'ajoute le contenue)
-// vous remarquerait que eleveManager n'est pas d'embler tester en faite le test final comprend la method testing qui va tester toute les methods si elle son bonne et si elle ne son pas vide , mais si je ne remplit pas la base de donné au préalable il y aura toujour une erreur c'est pour cela que le le test à la fin .pour tester les erreur vous pouvez essayer en vidant les table (eleves,joint_table_all et stat) puis  de mettre en commentaire les ligne 44($eleveManager->addEleve($eleve);), 57($statManager->addStat($stat);), 69($descrManager->addDescr($descr);) die sera activer et juste le message d'erreur s'affichera.
+// toute les breve son tester les une apres les autre (foreach array je fait une breve et en suite j'ajoute le contenue)
+// vous remarquerait que eleveManager n'est pas d'embler tester en faite le test final comprend la method testing qui va tester toute les methods si elle son bonne et si elle ne son pas vide , mais si je ne remplit pas la base de 
+// donné au préalable il y aura toujour une erreur c'est pour cela que le test à la fin .pour tester les erreur vous pouvez essayer en vidant les table (eleves,joint_table_all et stat) puis  de mettre en commentaire les 
+// ligne 44($eleveManager->addEleve($eleve);), 
+// 57($statManager->addStat($stat);), 
+// 69($descrManager->addDescr($descr);) 
+// die sera activer et juste le message d'erreur s'affichera.
 
 foreach ($listeEleve as $value) {
     $eleveManager = new EleveManager;
